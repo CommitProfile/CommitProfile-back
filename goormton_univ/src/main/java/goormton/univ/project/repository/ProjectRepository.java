f6975b6d20409f0e81c98842d7e4a05ccca7de3a
eunrimyu <eunrim2004@naver.com>
2025-07-07 22:05:34 -0600

    feat: ProjectRepository에 프로젝트 시작일 기준 연도 필터링 메서드 추가

--- goormton_univ/src/main/java/goormton/univ/project/repository/ProjectRepository.java
+++ goormton_univ/src/main/java/goormton/univ/project/repository/ProjectRepository.java
@@ -3,11 +3,12 @@ package goormton.univ.project.repository;
 import goormton.univ.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    List<Project> findByPeriodContaining(String keyword); //기간 조회
+    List<Project> findByStartDateBetween(LocalDate start, LocalDate end);
 
 
 }
